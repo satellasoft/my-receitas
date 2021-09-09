ac1dfcdc5c9b7ffff1d16a6460e5337c1ab5038e
SatellaSoft <=>
2021-09-08 23:36:17 -0300

    Fix: ajustes de autoloading para arquivos de configuração e funções

--- public/index.php
+++ public/index.php
@@ -1,7 +1,4 @@
 <?php
 require_once('..\vendor\autoload.php');
-require_once('..\app\config\global.php');
-require_once('..\app\functions\functions.php');
 
-use app\core\Router;
-(new Router());
+(new \app\core\Router());
